d886cfe6f6c222bc4196f9542f55b747b5c20a14
李聪 <lc182540@163.com>
2021-09-27 11:52:09 +0800

    feat: 修改以及筛选

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css'
 
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import { nanoid } from "nanoid"
 
 import ToDo from './components/ToDo'
@@ -9,16 +9,27 @@ import FilterButton from './components/FilterButton'
 
 function App(props) {
   const [tasks, setTask] = useState(props.tasks)
-  const tasksList =
-    tasks.map(it => (
-      <ToDo name={it.name} completed={it.completed} id={it.id} key={it.id} toggleTaskCompleted={toggleTaskCompleted} deleteTask={deleteTask} />
-    ))
+  const [filter, setFilter] = useState('All')
+
+  const FILTER_MAP = {
+    All: () => true,
+    Active: task => !task.completed,
+    Completed: task => task.completed
+  }
+
+  const FILTER_NAMES = Object.keys(FILTER_MAP)
+
+  const listHeadingRef = useRef(null)
+
+  const filterList = FILTER_NAMES.map(name => (
+    <FilterButton key={name} name={name} isPressed={name === filter}
+      setFilter={setFilter}></FilterButton>
+  ))
+
   const addTask = function (name) {
     const newTask = { id: `todo-${nanoid()}`, name, completed: false }
     setTask([...tasks, newTask])
   }
-  const tasksNoun = tasksList.length !== 1 ? 'tasks' : 'task'
-  const headingText = `${tasksList.length} ${tasksNoun} remaining`
   function toggleTaskCompleted(id) {
     const updateTasks = tasks.map(task => {
       if (id === task.id) {
@@ -30,19 +41,60 @@ function App(props) {
     })
     setTask(updateTasks)
   }
+
   function deleteTask(id) {
-    console.log(id)
+    const newTasks = tasks.filter(it => it.id !== id)
+    setTask(newTasks)
   }
+
+  function editTask(id, newName) {
+    const newTasks = tasks.map(it => {
+      if (it.id === id) {
+        return { ...it, name: newName }
+      }
+      return it
+    })
+    setTask(newTasks)
+  }
+
+  const tasksList = tasks.filter(FILTER_MAP[filter]).map(it => (
+    <ToDo
+      key={it.id}
+      name={it.name}
+      completed={it.completed}
+      id={it.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ))
+
+  const tasksNoun = tasksList.length !== 1 ? 'tasks' : 'task'
+  const headingText = `${tasksList.length} ${tasksNoun} remaining`
+
+  function usePrevious(value) {
+    const ref = useRef()
+    useEffect(() => {
+      ref.current = value
+    })
+    return ref.current
+  }
+
+  const prevTaskLength = usePrevious(tasks.length)
+  useEffect(() => {
+    if (tasks.length - prevTaskLength === -1) {
+      listHeadingRef.current.focus()
+    }
+  }, [tasks.length, prevTaskLength])
+
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask} />
       <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
+        {filterList}
       </div>
-      <h2 id="list-heading">
+      <h2 id="list-heading" ref={listHeadingRef}>
         {headingText}
       </h2>
       <ul
--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function FilterButtom() {
+export default function FilterButton(props) {
   return (
-    <button type="button" className="btn toggle-btn" aria-pressed="false">
+    <button type="button" className="btn toggle-btn" aria-pressed={props.isPressed} onClick={() => props.setFilter(props.name)}>
       <span className="visually-hidden">Show </span>
-      <span>Completed</span>
+      <span>{props.name}</span>
       <span className="visually-hidden"> tasks</span>
     </button>
   )
